782dde632dd0bf966e0cf577403ec8822d42ee3b
ronie0B03 <ronieB03@gmail.com>
2017-11-03 09:07:38 +0800

    1st Commit
    1st Commit, No Changes

---
+++ MachineLearning/Assets/Scripts/Cat.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    private bool initilized = false;
+    private Transform hex;
+
+    private neuralscript net;
+    private Rigidbody2D rBody;
+    private Material[] mats;
+    private SpriteRenderer[] newRenderer;
+    void Start()
+    {
+        //GetComponent<SpriteRenderer>().color = new Color(0.388235229f, 0.3372549f, 1f);
+        //renderer.color = new Color(0f, 0f, 0f, 1f); // Set to opaque black
+        rBody = GetComponent<Rigidbody2D>();
+        mats = new Material[transform.childCount];
+        newRenderer =  new SpriteRenderer[transform.childCount];
+        for (int i = 0; i < mats.Length; i++) { 
+            mats[i] = transform.GetChild(i).GetComponent<Renderer>().material;
+            newRenderer[i] = transform.GetChild(i).GetComponent<SpriteRenderer>();
+            newRenderer[i] = GetComponent<SpriteRenderer>();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (initilized == true)
+        {
+            float distance = Vector2.Distance(transform.position, hex.position);
+            GetComponent<SpriteRenderer>().color = new Color(distance / 5f, (2f - (distance / 5f)), (2f - (distance / 5f)));
+            if (distance > 20f)
+                distance = 20f;
+            for (int i = 0; i < mats.Length; i++)
+                GetComponent<SpriteRenderer>().color = new Color(distance / 20f, (1f - (distance / 20f)), (1f - (distance / 20f)));
+            //mats[i].color = new Color(distance / 20f, (1f - (distance / 20f)), (1f - (distance / 20f)));
+
+            float[] inputs = new float[1];
+
+
+            float angle = transform.eulerAngles.z % 360f;
+            if (angle < 0f)
+                angle += 360f;
+
+            Vector2 deltaVector = (hex.position - transform.position).normalized;
+
+
+            float rad = Mathf.Atan2(deltaVector.y, deltaVector.x);
+            rad *= Mathf.Rad2Deg;
+
+            rad = rad % 360;
+            if (rad < 0)
+            {
+                rad = 360 + rad;
+            }
+
+            rad = 90f - rad;
+            if (rad < 0f)
+            {
+                rad += 360f;
+            }
+            rad = 360 - rad;
+            rad -= angle;
+            if (rad < 0)
+                rad = 360 + rad;
+            if (rad >= 180f)
+            {
+                rad = 360 - rad;
+                rad *= -1f;
+            }
+            rad *= Mathf.Deg2Rad;
+
+            inputs[0] = rad / (Mathf.PI);
+
+
+            float[] output = net.FeedForward(inputs);
+
+            rBody.velocity = 2.5f * transform.up;
+            rBody.angularVelocity = 500f * output[0];
+
+            net.AddFitness((1f - Mathf.Abs(inputs[0])));
+        }
+    }
+
+    public void Init(neuralscript net, Transform hex)
+    {
+        this.hex = hex;
+        this.net = net;
+        initilized = true;
+    }
+
+
+}
---
+++ MachineLearning/Assets/Scripts/neuralscript.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections.Generic;
+using System;
+public class neuralscript : IComparable<neuralscript>
+{
+
+    private int[] layers;
+    private float[][] neurons;
+    private float[][][] weights;
+    private float fitness;
+    private Random random;
+
+    public neuralscript(int[] layers)
+    {
+        this.layers = new int[layers.Length];
+        for (int i=0; i<layers.Length; i++)
+        {
+            this.layers[i] = layers[i];
+        }
+
+        //random = new Random(System.DateTime.Today.Millisecond);
+
+        InitNeurons();
+        InitWeights();
+    }
+    
+    public neuralscript(neuralscript copyNetwork)
+    {
+        this.layers = new int[copyNetwork.layers.Length];
+        for (int i = 0; i < layers.Length; i++)
+        {
+            this.layers[i] = copyNetwork.layers[i];
+        }
+
+        InitNeurons();
+        InitWeights();
+        CopyWeights(copyNetwork.weights);
+    }
+
+    private void CopyWeights(float[][][] copyWeights)
+    {
+        for (int i=0; i<weights.Length; i++)
+        {
+            for (int j=0; j<weights[i].Length; j++)
+            {
+                for(int k=0; k < weights[i][j].Length; k++)
+                {
+                    weights[i][j][k] = copyWeights[i][j][k];
+                } 
+            }
+        }
+    }
+    private void InitNeurons()
+    {
+        //Neuron Initialization
+        List<float[]> neuronsList = new List<float[]>();
+
+        for (int i = 0; i<layers.Length; i++) //run through all layers
+        {
+            neuronsList.Add(new float[layers[i]]); //add layer to neuron list
+        }
+
+        neurons =  neuronsList.ToArray(); //convert list to array
+    }
+
+    private void InitWeights()
+    {
+        List<float[][]> weightsList = new List<float[][]>();
+
+        for (int i = 1; i < layers.Length; i++)
+        {
+            List<float[]> layerWeightList = new List<float[]>();
+
+            int neuronsInPreviousLayer = layers[i - 1];
+
+            for (int j = 0; j<neurons[i].Length; j++)
+            {
+                float[] neuronWeights = new float[neuronsInPreviousLayer];
+
+                for(int k = 0; k < neuronsInPreviousLayer; k++)
+                {
+                    //random weights
+                    //neuronWeights[k] = (float)random.NextDouble()-0.5f;
+                    neuronWeights[k] = UnityEngine.Random.Range(-0.5f, 0.5f);
+                }
+
+                layerWeightList.Add(neuronWeights);
+            }
+
+            weightsList.Add(layerWeightList.ToArray());
+        }
+
+        weights = weightsList.ToArray();
+    }
+
+    public float[] FeedForward(float[] inputs)
+    {
+        for(int i=0; i<inputs.Length; i++)
+        {
+            neurons[0][i] = inputs[i];
+        }
+
+        for(int i=1; i<layers.Length; i++)
+        {
+            for (int j=0; j<neurons[i].Length; j++)
+            {
+                float value = 0.25f;
+                for (int k=0; k<neurons[i-1].Length; k++)
+                {
+                    value += weights[i - 1][j][k] * neurons[i - 1][k];
+                }
+
+                neurons[i][j] = (float)Math.Tanh(value);
+            }
+        }
+        return neurons[neurons.Length-1];
+    }
+
+    public void Mutate()
+    {
+        for(int i = 0; i<weights.Length; i++)
+        {
+            for (int j=0; j<weights[i].Length; j++)
+            {
+                for(int k =0; k<weights[i][j].Length; k++)
+                {
+                    float weight = weights[i][j][k];
+
+                    //where is where mutation kicks in
+                    //float randomNumber = (float)random.NextDouble() * 1000f;
+                    float randomNumber = UnityEngine.Random.Range(0f, 100f);
+
+                    if (randomNumber<=2f)
+                    {
+                        weight *= -1f;
+                    }
+                    else if (randomNumber<=4f)
+                    {
+                        weight = UnityEngine.Random.Range(-0.5f, 0.5f);
+                    }
+                    else if (randomNumber <= 6f)
+                    {
+                        float factor = UnityEngine.Random.Range(0f, 1f)+1f;
+                        weight *= factor;
+                    }
+                    else if (randomNumber <= 8f)
+                    {
+                        float factor = UnityEngine.Random.Range(0f, 1f);
+                        weight *= factor;
+                    }
+
+                    weights[i][j][k] = weight;
+                }
+            }
+        }
+    }
+
+    public void AddFitness(float fit)
+    {
+        fitness += fit;
+    }
+
+    public void SetFitness(float fit)
+    {
+        fitness = fit;
+    }
+
+    public float GetFitness()
+    {
+        return fitness;
+    }
+
+    public int CompareTo(neuralscript other)
+    {
+        if (other == null) return 1;
+
+        if (fitness > other.fitness)
+            return 1;
+        else if (fitness < other.fitness)
+            return -1;
+        else
+            return 0;
+    }
+}
